d56d56540b5fead0bd8230c7efe1ab4cd64987c1
db12334 <118720999+db12334@users.noreply.github.com>
2023-11-13 15:08:48 +1100

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,5 @@
+function addUser() {
+    player1_name = document.getElementById("player1_name_input").value;
+    player1_name = document.getElementById("player1_name_input").value;
+    window.location = "quiz_game_page.html"
+}
